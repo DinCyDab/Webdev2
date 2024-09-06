03af12a6e185d7e9aee7b6e859cb88831ca60511
DinCyDab <19103291@usc.edu.ph>
2024-09-06 22:37:51 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="src/css/styles.css">
+    </head>
+    <body>
+        <div class="container">
+            <div class="grid">
+                <div class="navigation-tab">
+                    <?php 
+                        include "src/html/navigation-bar.html";
+                    ?>
+                </div>
+                <div class="header">
+                    <?php 
+                        include "src/html/header.html";
+                    ?>
+                </div>
+                <div class="content">
+                    <?php 
+                        include "src/html/dashboard-content.html";
+                    ?>
+                    <!-- Delete this Later -->
+                    <h1>Test Overflow</h1>
+                    <h1>Test Overflow</h1>
+                    <h1>Test Overflow</h1>
+                    <h1>Test Overflow</h1>
+                    <h1>Test Overflow</h1>
+                    <h1>Test Overflow</h1>
+                    <!-- Delete this Later -->
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
